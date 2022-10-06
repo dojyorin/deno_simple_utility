9d1e43c69236c7edfaefc316dbb5cce28fbf34a7
Kazuki Ota <ota_droid@live.jp>
2022-10-06 17:28:03 +0900

    Update test code.

---
+++ test/base64_test.ts
@@ -0,0 +1,23 @@
+import {assertEquals} from "https://deno.land/std@0.158.0/testing/asserts.ts";
+import {base64Encode, base64Decode} from "../src/base64.ts";
+
+const sampleByte = new Uint8Array([1, 2, 3, 4]);
+const sampleCode = "AQIDBA==";
+
+Deno.test({
+    name: "Encode.",
+    fn(){
+        const result = base64Encode(sampleByte);
+
+        assertEquals(result, sampleCode);
+    }
+});
+
+Deno.test({
+    name: "Decode.",
+    fn(){
+        const result = base64Encode(sampleCode);
+
+        assertEquals(result, sampleByte);
+    }
+});
