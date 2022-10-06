139d1e02d736e0b8840201048690bc25e7ba1dd1
Kazuki Ota <ota_droid@live.jp>
2022-10-06 16:41:15 +0900

    WIP.

--- src/minipack.ts
+++ src/minipack.ts
@@ -3,15 +3,15 @@ const HEADER_HASH = 32;
 const HEADER_NAME = 256;
 const HEADERS = HEADER_SIZE + HEADER_HASH + HEADER_NAME;
 
-async function sha256(data:ArrayBuffer){
+async function deriveHash(data:ArrayBuffer){
     return await crypto.subtle.digest("SHA-256", data);
 }
 
-function bString(data:ArrayBuffer){
+function byteString(data:ArrayBuffer){
     return new Uint8Array(data).toString();
 }
 
-async function minipackEncode(files:File[]){
+export async function minipackEncode(files:File[]){
     if(files.some(({size, name}) => size > (0x100 ** HEADER_SIZE) || new TextEncoder().encode(name).byteLength > HEADER_NAME)){
         throw new Error();
     }
@@ -28,7 +28,7 @@ async function minipackEncode(files:File[]){
 
         archive.set(new Uint8Array(dv.buffer), offset);
         offset += HEADER_SIZE;
-        archive.set(new Uint8Array(await sha256(data)), offset);
+        archive.set(new Uint8Array(await deriveHash(data)), offset);
         offset += HEADER_HASH;
         archive.set(new TextEncoder().encode(file.name), offset);
         offset += HEADER_NAME;
@@ -39,7 +39,7 @@ async function minipackEncode(files:File[]){
     return archive.buffer;
 }
 
-async function minipackDecode(archive:ArrayBuffer){
+export async function minipackDecode(archive:ArrayBuffer){
     const files:File[] = [];
 
     let offset = 0;
@@ -50,7 +50,7 @@ async function minipackDecode(archive:ArrayBuffer){
         const name = new TextDecoder().decode(archive.slice(offset, offset += HEADER_NAME)).replace(/\0+$/, "");
         const data = archive.slice(offset, offset += size);
 
-        if(bString(hash) !== bString(await sha256(data))){
+        if(byteString(hash) !== byteString(await deriveHash(data))){
             throw new Error();
         }
 
