47e83749284bcb63d03ce6da323d4680eab30308
Kazuki Ota <ota_droid@live.jp>
2022-10-20 12:56:58 +0900

    split common stream code.

--- src/deflate.ts
+++ src/deflate.ts
@@ -1,10 +1,14 @@
+async function deflate(data:Uint8Array, ctx:CompressionStream|DecompressionStream){
+    return new Uint8Array(await new Response(new Blob([data]).stream().pipeThrough(ctx)).arrayBuffer());
+}
+
 /**
 * Compresses raw binary in "deflate" format (RFC1951 compliant).
 * It does not include header information like "gzip" (RFC1952) or "zlib" (RFC1950) as it does purely "compression only".
 * @param data The byte buffer.
 **/
 export async function deflateEncode(data:Uint8Array){
-    return new Uint8Array(await new Response(new Blob([data]).stream().pipeThrough(new CompressionStream("deflate-raw"))).arrayBuffer());
+    return await deflate(data, new CompressionStream("deflate-raw"));
 }
 
 /**
@@ -13,5 +17,5 @@ export async function deflateEncode(data:Uint8Array){
 * @param data The byte buffer.
 **/
 export async function deflateDecode(data:Uint8Array){
-    return new Uint8Array(await new Response(new Blob([data]).stream().pipeThrough(new DecompressionStream("deflate-raw"))).arrayBuffer());
+    return await deflate(data, new DecompressionStream("deflate-raw"));
 }
