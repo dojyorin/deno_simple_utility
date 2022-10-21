c694e40896009856e37f83d8c339ad23111f2b70
Kazuki Ota <ota_droid@live.jp>
2022-10-21 14:56:07 +0900

    fix minipack header.

--- src/minipack.ts
+++ src/minipack.ts
@@ -3,10 +3,13 @@
 //   [BodyHash:  32-byte]
 //   [FileName: 256-byte]
 // Total: 292-byte
-const HEADER_SIZE = 4;
-const HEADER_HASH = 32;
-const HEADER_NAME = 256;
-const HEADERS = HEADER_SIZE + HEADER_HASH + HEADER_NAME;
+const headerStruct = <const>{
+    body: 4,
+    hash: 32,
+    name: 256
+};
+
+const headerTotal = Object.values(headerStruct).reduce((a, c) => a + c, 0);
 
 async function sha256(data:Uint8Array){
     return new Uint8Array(await crypto.subtle.digest("SHA-256", data));
@@ -34,7 +37,7 @@ function byte2text(data:Uint8Array){
 * @param files Array of file objects.
 **/
 export async function minipackEncode(files:File[]){
-    const archive = new Uint8Array(files.reduce((a, {size}) => a + size + HEADERS, 0));
+    const archive = new Uint8Array(files.reduce((a, {size}) => a + size + headerTotal, 0));
 
     let offset = 0;
 
@@ -42,11 +45,11 @@ export async function minipackEncode(files:File[]){
         const data = new Uint8Array(await file.arrayBuffer());
 
         new DataView(archive.buffer, offset).setUint32(0, file.size);
-        offset += HEADER_SIZE;
+        offset += headerStruct.body;
         archive.set(await sha256(data), offset);
-        offset += HEADER_HASH;
-        archive.set(text2byte(file.name).slice(0, 256), offset);
-        offset += HEADER_NAME;
+        offset += headerStruct.hash;
+        archive.set(text2byte(file.name).subarray(0, headerStruct.name), offset);
+        offset += headerStruct.name;
         archive.set(data, offset);
         offset += file.size;
     }
@@ -74,8 +77,8 @@ export async function minipackDecode(archive:Uint8Array){
 
     while(offset < archive.byteLength){
         const size = new DataView(archive.buffer, offset).getUint32(0);
-        const hash = archive.subarray(offset += HEADER_SIZE, offset += HEADER_HASH);
-        const name = byte2text(archive.subarray(offset, offset += HEADER_NAME)).replace(/\0+$/, "");
+        const hash = archive.subarray(offset += headerStruct.body, offset += headerStruct.hash);
+        const name = byte2text(archive.subarray(offset, offset += headerStruct.name)).replace(/\0+$/, "");
         const data = archive.subarray(offset, offset += size);
 
         if(hash.toString() !== (await sha256(data)).toString()){
