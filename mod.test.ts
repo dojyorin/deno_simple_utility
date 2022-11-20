71540559a06773bc3897a33a1dd355450af16a8d
Kazuki Ota <ota_droid@live.jp>
2022-11-20 19:00:55 +0900

    add module.

--- mod.test.ts
+++ mod.test.ts
@@ -1,4 +1,6 @@
 import "./test/base64.test.ts";
+import "./test/date.test.ts";
 import "./test/deflate.test.ts";
 import "./test/fetch.test.ts";
 import "./test/minipack.test.ts";
+import "./test/text.test.ts";
