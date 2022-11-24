08ee35404d717050aab4fba0e304bc86c87db1f5
Kazuki Ota <ota_droid@live.jp>
2022-11-25 01:11:03 +0900

    fix declare.

--- src/web.d.ts
+++ src/web.d.ts
@@ -1,7 +1,7 @@
 /**
 * Possible types of JSON.
 */
-export type JsonStruct = string | number | boolean | null | JsonStruct[] | {[key: string]: JsonStruct;};
+export type JsonStruct = string | number | boolean | null | JsonStruct[] | {[key in string]: JsonStruct};
 
 /**
 * Possible input types for `URLSearchParams`.
