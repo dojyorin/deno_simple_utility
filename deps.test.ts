c569ef3dc186bc3dd48cd11e08b55ac85c1361bc
Kazuki Ota <ota_droid@live.jp>
2022-11-25 16:02:57 +0900

    update deps.

--- deps.test.ts
+++ deps.test.ts
@@ -1,3 +1,3 @@
-export {assertEquals} from "https://deno.land/std@0.165.0/testing/asserts.ts";
-export {dirname, fromFileUrl} from "https://deno.land/std@0.165.0/path/mod.ts";
-export {serve} from "https://deno.land/std@0.165.0/http/mod.ts";
+export {assertEquals} from "https://deno.land/std@0.166.0/testing/asserts.ts";
+export {dirname, fromFileUrl} from "https://deno.land/std@0.166.0/path/mod.ts";
+export {serve} from "https://deno.land/std@0.166.0/http/mod.ts";
--- deps.ts
+++ deps.ts
@@ -1,2 +1,2 @@
-export {} from "https://deno.land/std@0.165.0/bytes/mod.ts";
-export {dirname, fromFileUrl} from "https://deno.land/std@0.165.0/path/mod.ts";
+export {} from "https://deno.land/std@0.166.0/bytes/mod.ts";
+export {dirname, fromFileUrl} from "https://deno.land/std@0.166.0/path/mod.ts";
