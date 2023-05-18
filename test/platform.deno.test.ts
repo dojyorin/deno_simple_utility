e64b0eebe220b4531365a182f062decc028cc284
Kazuki Ota <ota_droid@live.jp>
2023-05-18 17:38:09 +0900

    fix mehtod name.

--- test/platform.deno.test.ts
+++ test/platform.deno.test.ts
@@ -1,11 +1,11 @@
 import {assertEquals} from "../deps.test.ts";
-import {isWindows} from "../src/platform.deno.ts";
+import {isWin} from "../src/platform.deno.ts";
 
 Deno.test({
     ignore: Deno.build.os !== "windows",
     name: "Platform: Windows",
     async fn(){
-        assertEquals(isWindows(), true);
+        assertEquals(isWin(), true);
     }
 });
 
@@ -13,6 +13,6 @@ Deno.test({
     ignore: Deno.build.os === "windows",
     name: "Platform: Posix",
     async fn(){
-        assertEquals(isWindows(), false);
+        assertEquals(isWin(), false);
     }
 });
