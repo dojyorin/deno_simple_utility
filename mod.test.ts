b480b1f7f1a3d0d523f90982b0f7c2e745e22730
dojyorin <ota_droid@live.jp>
2023-07-16 01:13:08 +0900

    change file name.

--- mod.test.ts
+++ mod.test.ts
@@ -1,11 +1,11 @@
 import "./test/base64.test.ts";
 import "./test/crypto.test.ts";
+import "./test/deep.test.ts";
 import "./test/deflate.test.ts";
 import "./test/fetch.test.ts";
 import "./test/json.deno.test.ts";
 import "./test/log.deno.test.ts";
 import "./test/minipack.test.ts";
-import "./test/object.test.ts";
 import "./test/path.deno.test.ts";
 import "./test/platform.deno.test.ts";
 import "./test/process.deno.test.ts";
--- mod.ts
+++ mod.ts
@@ -1,9 +1,9 @@
 export * from "./src/base64.ts";
 export * from "./src/crypto.ts";
+export * from "./src/deep.ts";
 export * from "./src/deflate.ts";
 export * from "./src/fetch.ts";
 export * from "./src/minipack.ts";
-export * from "./src/object.ts";
 export * from "./src/text.ts";
 export * from "./src/time.ts";
 
