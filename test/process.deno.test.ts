a54414613232ebe9d03526979d8129cc63401ffe
dojyorin <ota_droid@live.jp>
2023-07-27 17:09:16 +0900

    fix jsdoc.

--- test/process.deno.test.ts
+++ test/process.deno.test.ts
@@ -2,7 +2,6 @@ import {assertEquals} from "../deps.test.ts";
 import {runCommand} from "../src/process.deno.ts";
 
 Deno.test({
-    ignore: Deno.build.os !== "windows",
     name: "Process: Run (no args)",
     async fn(){
         const result = await runCommand("echo", "abcdefg");
