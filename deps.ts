0aa44a95f81870470eee3ecdb30bb769c428b0d6
dojyorin <ota_droid@live.jp>
2023-11-22 14:35:23 +0900

    fix type.

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export {dirname, fromFileUrl} from "https://deno.land/std@0.203.0/path/mod.ts";
-export {Logger, handlers} from "https://deno.land/std@0.203.0/log/mod.ts";
-export {format} from "https://deno.land/std@0.203.0/datetime/mod.ts";
+export {dirname, fromFileUrl} from "https://deno.land/std@0.207.0/path/mod.ts";
+export {Logger, handlers} from "https://deno.land/std@0.207.0/log/mod.ts";
+export {format} from "https://deno.land/std@0.207.0/datetime/mod.ts";
