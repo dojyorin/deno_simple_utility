a6f9735cd3e0c547a8b9fb92f52539d2136a96a6
dojyorin <ota_droid@live.jp>
2023-12-08 09:18:51 +0900

    remove configload.

--- test/json.deno.test.ts
+++ test/json.deno.test.ts
@@ -1,5 +1,5 @@
 import {assertEquals} from "../deps.test.ts";
-import {jsonRead, jsonWrite, jsonLoad, configLoad} from "../src/json.deno.ts";
+import {jsonRead, jsonWrite, jsonLoad} from "../src/json.deno.ts";
 
 const object = <const>{
     id: "22d8b040-8a63-46a0-8df6-0f508a778689",
@@ -25,17 +25,13 @@ Deno.test({
     async fn(){
         const path = "./resource.json";
 
-        const result01 = await jsonLoad(path, object);
-        const result02 = await configLoad(object);
-        assertEquals(result01, object);
-        assertEquals(result02, object);
+        const result1 = await jsonLoad(path, object);
+        assertEquals(result1, object);
 
         const modify = structuredClone(object);
         modify.name = "Ninja Slayer";
 
-        const result11 = await jsonLoad(path, modify);
-        const result12 = await configLoad(modify);
-        assertEquals(result11, object);
-        assertEquals(result12, object);
+        const result2 = await jsonLoad(path, modify);
+        assertEquals(result2, object);
     }
 });
