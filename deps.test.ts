d708423d4b78d691c3db587973008bee807eefcb
dojyorin <ota_droid@live.jp>
2023-12-14 16:56:03 +0900

    update version.

--- deps.test.ts
+++ deps.test.ts
@@ -1,3 +1,3 @@
-export {assertEquals} from "https://deno.land/std@0.208.0/assert/mod.ts";
-export {dirname, fromFileUrl} from "https://deno.land/std@0.208.0/path/mod.ts";
-export {exists} from "https://deno.land/std@0.208.0/fs/mod.ts";
+export {assertEquals} from "https://deno.land/std@0.209.0/assert/mod.ts";
+export {dirname, fromFileUrl} from "https://deno.land/std@0.209.0/path/mod.ts";
+export {exists} from "https://deno.land/std@0.209.0/fs/mod.ts";
