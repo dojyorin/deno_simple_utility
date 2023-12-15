11012cb386d2bb81c5433ea5b1271e0cbc10badf
dojyorin <ota_droid@live.jp>
2023-12-15 16:20:02 +0900

    feat: stream.

--- mod.test.ts
+++ mod.test.ts
@@ -12,5 +12,6 @@ import "./test/minipack.test.ts";
 import "./test/path.deno.test.ts";
 import "./test/platform.deno.test.ts";
 import "./test/process.deno.test.ts";
+import "./test/stream.test.ts";
 import "./test/text.test.ts";
 import "./test/time.test.ts";
