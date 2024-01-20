a52e3ad5546c5e65e1ec20804dd29ae0a72a08aa
dojyorin <ota_droid@live.jp>
2024-01-21 03:32:10 +0900

    fix test code.

--- test/worker.test.ts
+++ test/worker.test.ts
@@ -2,7 +2,7 @@ import {assertEquals} from "../deps.test.ts";
 import {createTask} from "../src/worker.ts";
 
 const sample1 = new Uint8Array([1, 2, 3, 4]);
-const sample2 = new Uint8Array([1, 4, 6, 8]);
+const sample2 = new Uint8Array([2, 4, 6, 8]);
 
 Deno.test({
     name: "Worker: Create Task.",
