7c6bb13eb6c08ec5e90b1f49ddbc415670852500
dojyorin <ota_droid@live.jp>
2024-01-21 03:23:36 +0900

    update deps.

--- deps.test.ts
+++ deps.test.ts
@@ -1,3 +1,3 @@
-export {assertEquals} from "https://deno.land/std@0.210.0/assert/mod.ts";
-export {dirname, fromFileUrl} from "https://deno.land/std@0.210.0/path/mod.ts";
-export {exists} from "https://deno.land/std@0.210.0/fs/mod.ts";
+export {assertEquals} from "https://deno.land/std@0.212.0/assert/mod.ts";
+export {dirname, fromFileUrl} from "https://deno.land/std@0.212.0/path/mod.ts";
+export {exists} from "https://deno.land/std@0.212.0/fs/mod.ts";
--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export {dirname, fromFileUrl} from "https://deno.land/std@0.210.0/path/mod.ts";
-export {Logger, handlers} from "https://deno.land/std@0.210.0/log/mod.ts";
-export {format} from "https://deno.land/std@0.210.0/datetime/mod.ts";
+export {dirname, fromFileUrl} from "https://deno.land/std@0.212.0/path/mod.ts";
+export {Logger, handlers} from "https://deno.land/std@0.212.0/log/mod.ts";
+export {format} from "https://deno.land/std@0.212.0/datetime/mod.ts";
