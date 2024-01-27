79683fe01b26af4d78563262dee4b2e0ee05e715
dojyorin <ota_droid@live.jp>
2024-01-28 04:31:45 +0900

    integrate export.

--- mod.full.ts
+++ mod.full.ts
@@ -1,24 +1,8 @@
-export * from "./src/pure/base64.ts";
-export * from "./src/pure/byte.ts";
-export * from "./src/pure/crypto.ts";
-export * from "./src/pure/deep.ts";
-export * from "./src/pure/deflate.ts";
-export * from "./src/pure/fetch.ts";
-export * from "./src/pure/minipack.ts";
-export * from "./src/pure/primitive.ts";
-export * from "./src/pure/stream.ts";
-export * from "./src/pure/text.ts";
-export * from "./src/pure/time.ts";
-export * from "./src/pure/worker.ts";
+export * from "./mod.ts";
 
 export * from "./src/pure_ext/csv.ts";
 export * from "./src/pure_ext/excel.ts";
 export * from "./src/pure_ext/zip.ts";
-
-export * from "./src/deno/json.ts";
-export * from "./src/deno/os.ts";
-export * from "./src/deno/path.ts";
-export * from "./src/deno/process.ts";
 
 export * from "./src/deno_ext/dom.ts";
 export * from "./src/deno_ext/log.ts";
