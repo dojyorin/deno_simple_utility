96d8d6a5ffe74c866940f1b899570e5c0a49b78e
Kazuki Ota <ota_droid@live.jp>
2024-01-31 17:36:46 +0900

    update docs and deps. (#92)

--- deps.pure_ext.ts
+++ deps.pure_ext.ts
@@ -1,6 +1,6 @@
 export {parse, stringify} from "https://deno.land/std@0.213.0/csv/mod.ts";
 
-export {ZipReader, ZipWriter, Uint8ArrayReader, Uint8ArrayWriter} from "https://deno.land/x/zipjs@v2.7.32/index.js";
+export {ZipReader, ZipWriter, Uint8ArrayReader, Uint8ArrayWriter} from "https://deno.land/x/zipjs@v2.7.34/index.js";
 
 // @deno-types="https://cdn.sheetjs.com/xlsx-0.20.1/package/types/index.d.ts"
 export {type WorkBook as RawWorkBook, type WorkSheet as RawWorkSheet, type CellObject as RawWorkCell, set_cptable, read as xlsxRead, write as xlsxWrite, utils as xlsxUtil} from "https://cdn.sheetjs.com/xlsx-0.20.1/package/xlsx.mjs";
--- deps.test.ts
+++ deps.test.ts
@@ -3,6 +3,6 @@ export {dirname, fromFileUrl} from "https://deno.land/std@0.213.0/path/mod.ts";
 export {exists} from "https://deno.land/std@0.213.0/fs/mod.ts";
 export {delay} from "https://deno.land/std@0.213.0/async/mod.ts";
 
-export {DOMParser} from "https://deno.land/x/deno_dom@v0.1.43/deno-dom-wasm.ts";
+export {DOMParser} from "https://deno.land/x/deno_dom@v0.1.45/deno-dom-wasm.ts";
 
 export {init as smtpTest} from "npm:smtp-tester@2.1.0";
