c85193fe2ad24b8352053e6fa19df448ee9fa07c
dojyorin <ota_droid@live.jp>
2024-05-23 19:10:30 +0900

    fix dataurl

--- src/pure/worker.ts
+++ src/pure/worker.ts
@@ -1,4 +1,4 @@
-import {base64EncodeDataURL} from "./base64.ts";
+import {base64Encode} from "./base64.ts";
 import {textEncode} from "./text.ts";
 
 interface TaskMessage<T extends unknown> {
@@ -45,7 +45,7 @@ export function workerTask<T extends unknown, K extends unknown>(task:TaskAction
 
     return (message, transfers)=>{
         return new Promise<K>((res, rej)=>{
-            const worker = new Worker(base64EncodeDataURL(textEncode(script), "text/javascript"), {
+            const worker = new Worker(`data:text/javascript;base64,${base64Encode(textEncode(script))}`, {
                 type: "module"
             });
 
