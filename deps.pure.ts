f96824fd8d69030e982b86066861894b363fcf6e
dojyorin <ota_droid@live.jp>
2024-06-30 20:25:13 +0900

    jsr support.

--- deps.pure.ts
+++ deps.pure.ts
@@ -1,7 +1,5 @@
-// @deno-types="https://deno.land/std@0.224.0/csv/mod.ts"
-export {parse, stringify} from "https://esm.sh/gh/denoland/deno_std@0.224.0/csv/mod.ts?bundle&target=esnext";
-// @deno-types="https://deno.land/x/zipjs@v2.7.45/index.d.ts"
-export {ZipReader, ZipWriter, Uint8ArrayReader, Uint8ArrayWriter} from "https://esm.sh/gh/gildas-lormeau/zip.js@v2.7.45/index.js?bundle&target=esnext";
+export {parse, stringify} from "https://esm.sh/jsr/@std/csv@0.224.3?bundle&target=esnext";
+export {ZipReader, ZipWriter, Uint8ArrayReader, Uint8ArrayWriter} from "https://esm.sh/jsr/@zip-js/zip-js@2.7.45?bundle&target=esnext";
 
 // @deno-types="https://cdn.sheetjs.com/xlsx-0.20.2/package/types/index.d.ts"
 export {type WorkBook as RawWorkBook, type WorkSheet as RawWorkSheet, type CellObject as RawWorkCell, set_cptable, read as xlsxRead, write as xlsxWrite, utils as xlsxUtil} from "https://cdn.sheetjs.com/xlsx-0.20.2/package/xlsx.mjs";
--- deps.test.ts
+++ deps.test.ts
@@ -1,8 +1,7 @@
-export {assertEquals} from "https://deno.land/std@0.224.0/assert/mod.ts";
-export {dirname, fromFileUrl} from "https://deno.land/std@0.224.0/path/mod.ts";
-export {exists} from "https://deno.land/std@0.224.0/fs/mod.ts";
-export {delay} from "https://deno.land/std@0.224.0/async/mod.ts";
-
-export {DOMParser} from "https://deno.land/x/deno_dom@v0.1.46/deno-dom-wasm.ts";
+export {assertEquals} from "jsr:@std/assert@0.226.0";
+export {dirname, fromFileUrl} from "jsr:@std/path@0.225.2";
+export {exists} from "jsr:@std/fs@0.229.3";
+export {delay} from "jsr:@std/async@0.224.2";
+export {DOMParser} from "jsr:@b-fuze/deno-dom@0.1.47";
 
 export {init as smtpTest} from "npm:smtp-tester@2.1.0";
