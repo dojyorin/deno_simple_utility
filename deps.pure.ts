3798ad03f8cd6f4b89c70e15ef267a899651b61e
dojyorin <ota_droid@live.jp>
2024-08-23 15:38:01 +0900

    update

--- deps.pure.ts
+++ deps.pure.ts
@@ -1,6 +1,6 @@
-export {parse, stringify} from "https://esm.sh/jsr/@std/csv@0.224.3?bundle&target=esnext";
-export {ZipReader, ZipWriter, Uint8ArrayReader, Uint8ArrayWriter} from "https://esm.sh/jsr/@zip-js/zip-js@2.7.45?bundle&target=esnext";
+export {parse, stringify} from "https://esm.sh/jsr/@std/csv@1.0.1?bundle&target=esnext";
+export {ZipReader, ZipWriter, Uint8ArrayReader, Uint8ArrayWriter} from "https://esm.sh/jsr/@zip-js/zip-js@2.7.51?bundle&target=esnext";
 
-// @deno-types="https://cdn.sheetjs.com/xlsx-0.20.2/package/types/index.d.ts"
-export {type WorkBook as RawWorkBook, type WorkSheet as RawWorkSheet, type CellObject as RawWorkCell, set_cptable, read as sheetRead, write as sheetWrite, utils as sheetUtil} from "https://cdn.sheetjs.com/xlsx-0.20.2/package/xlsx.mjs";
-export * as excelcp from "https://cdn.sheetjs.com/xlsx-0.20.2/package/dist/cpexcel.full.mjs";
+// @deno-types="https://cdn.sheetjs.com/xlsx-0.20.3/package/types/index.d.ts"
+export {type WorkBook as RawWorkBook, type WorkSheet as RawWorkSheet, type CellObject as RawWorkCell, set_cptable, read as sheetRead, write as sheetWrite, utils as sheetUtil} from "https://cdn.sheetjs.com/xlsx-0.20.3/package/xlsx.mjs";
+export * as excelcp from "https://cdn.sheetjs.com/xlsx-0.20.3/package/dist/cpexcel.full.mjs";
